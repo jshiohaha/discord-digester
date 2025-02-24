0e45dc732a9abb0a5e4b98b8d964ed8e82e0bc00
shio 🚀 <jlshiohira23@gmail.com>
2025-02-24 12:19:24 -0700

    add ssl configuration to db pool constructor

--- src/db.ts
+++ src/db.ts
@@ -14,6 +14,7 @@ import { messages } from "./schema/messages";
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
 });
 
 const db = drizzle(pool, {
