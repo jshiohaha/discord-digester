93bb524ee0b0d8e7605598434764e9237719aae1
shio 🚀 <jlshiohira23@gmail.com>
2025-02-27 07:51:08 +0900

    re-add reply to relation

--- src/schema/messages.ts
+++ src/schema/messages.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import {
     index,
     jsonb,
@@ -33,10 +34,9 @@ export const messages = pgTable(
     })
 );
 
-// todo: does this exist?
-// export const replyToRelation = relations(messages, ({ one }) => ({
-//     replyTo: one(messages, {
-//         fields: [messages.replyTo],
-//         references: [messages.messageId],
-//     }),
-// }));
+export const replyToRelation = relations(messages, ({ one }) => ({
+    replyTo: one(messages, {
+        fields: [messages.replyTo],
+        references: [messages.messageId],
+    }),
+}));
